8e710fc60b8588856aca5e52bec1e54722b6583b
zhangzhihao <710897522@qq.com>
2021-08-10 17:46:41 +0800

    路由

---
+++ Route/web.php
@@ -0,0 +1,7 @@
+<?php
+use Lib\Facade\Route;
+
+Route::addRule('index','index/index');
+Route::addRule('cart','index/cart');
+Route::addRule('quick','sort/quick');
+Route::addRule('count','sort/count');
